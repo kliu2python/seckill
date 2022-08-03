6974f01da7317ac3746a6d6e53f53e402acb26f4
Jiahao Liu <kliuautomationeng1@gmail.con>
2022-08-03 16:35:19 -0700

    first commit

---
+++ src/main/java/com/ljiahao/app/seckill/db/dao/SeckillActivityDao.java
@@ -0,0 +1,16 @@
+package com.ljiahao.app.seckill.db.dao;
+
+import com.ljiahao.app.seckill.db.po.SeckillActivity;
+
+import java.util.List;
+
+public interface SeckillActivityDao {
+
+    public List<SeckillActivity> querySeckillActivitysByStatus(int activityStatus);
+
+    public void inertSeckillActivity(SeckillActivity seckillActivity);
+
+    public SeckillActivity querySeckillActivityById(long activityId);
+
+    public void updateSeckillActivity(SeckillActivity seckillActivity);
+}
---
+++ src/main/java/com/ljiahao/app/seckill/db/dao/SeckillCommodityDao.java
@@ -0,0 +1,8 @@
+package com.ljiahao.app.seckill.db.dao;
+
+import com.ljiahao.app.seckill.db.po.SeckillCommodity;
+
+public interface SeckillCommodityDao {
+
+    public SeckillCommodity querySeckillCommodityById(long commodityId);
+}
